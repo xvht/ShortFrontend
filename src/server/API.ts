24eac6761a5e7a25753bb6d253543a74a7ca97a0
devfxx <fxx@kty.lol>
2024-07-16 06:49:43 +0300

    chore: Improve error handling and URL validation in Shorten API function

--- src/server/API.ts
+++ src/server/API.ts
@@ -1,17 +1,25 @@
 "use server";
 
 import { env } from "@/env";
-import { type Shorten } from "@/types/Shorten";
+import { type Response } from "@/types/Shorten";
+import { z } from "zod";
 
 export default async function Shorten(link: string) {
-  const res = await fetch(`${env.API_URL}/new`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ url: link }),
-  });
+  // make sure the link is a valid URL
+  try {
+    if (!z.string().url().parse(link)) return;
 
-  const data = (await res.json()) as Shorten;
-  return data;
+    const res = await fetch(`${env.API_URL}/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: link }),
+    });
+
+    const data = (await res.json()) as Response;
+    return data;
+  } catch (e) {
+    return;
+  }
 }
